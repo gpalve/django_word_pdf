333d55e864d7792a031d70f5c9da611951f1a8df
Ganesh Palve <gpalve@gmail.com>
2023-02-28 18:06:08 +0530

    Added 2 forms separately on same page

--- djconverter/models.py
+++ djconverter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploads/')
+    # name = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
         app_label = 'djconverter'
